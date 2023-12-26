e2da5190ab09ae7f61dd83f7e9980752feeb8409
seuno <electrokai1@gmail.com>
2023-12-26 13:53:41 +0800

    fixed

--- script.py
+++ script.py
@@ -5,4 +5,4 @@ cmd = 'pyinstaller --onefile --add-data="templates;templates" --add-data="static
 code = subprocess.run(cmd, shell=True)
 
 if code.returncode == 0:
-    os.replace("dist/__main__.exe", "mupdog.exe")
+    os.replace("dist/__main__.exe", "dist/mupdog.exe")
